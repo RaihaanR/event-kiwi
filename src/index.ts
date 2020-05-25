4d441d7085f6dc81e7f4c7207b4f769dbfb47757
Devesh Erda <devesh.erda18@imperial.ac.uk>
2020-05-25 17:29:19 +0100

    Formatting for consistency

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import pgPromise from 'pg-promise';
 
 const app = express();
@@ -15,61 +15,61 @@ const dbOptions = {
 };
 const db = pgp(dbOptions);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.get("/select1", (req, res) => {
+app.get('/select1', (req, res) => {
   db.any('SELECT * FROM test.table1')
     .then((data: any) => {
       res.send(data);
     })
     .catch((error: any) => {
-      console.log("Error: " + error);
+      console.log('Error: ' + error);
     });
 });
 
-app.get("/select1/:col", (req, res) => {
+app.get('/select1/:col', (req, res) => {
   db.any('SELECT $1:name FROM test.table1', [req.params['col']])
     .then((data: any) => {
       res.send(data);
     })
     .catch((error: any) => {
-      console.log("Error: " + error);
+      console.log('Error: ' + error);
     });
 });
 
-app.get("/select2", (req, res) => {
+app.get('/select2', (req, res) => {
   db.any('SELECT * FROM test.table2')
     .then((data: any) => {
       res.send(data);
     })
     .catch((error: any) => {
-      console.log("Error: " + error);
+      console.log('Error: ' + error);
     });
 });
 
-app.get("/select2/:col", (req, res) => {
+app.get('/select2/:col', (req, res) => {
   db.any('SELECT $1:name FROM test.table2', [req.params['col']])
     .then((data: any) => {
       res.send(data);
     })
     .catch((error: any) => {
-      console.log("Error: " + error);
+      console.log('Error: ' + error);
     });
 });
 
-app.get("/join", (req, res) => {
+app.get('/join', (req, res) => {
   db.any('SELECT * FROM test.table1 NATURAL JOIN test.table2')
     .then((data: any) => {
       res.send(data);
     })
     .catch((error: any) => {
-      console.log("Error: " + error);
+      console.log('Error: ' + error);
     });
 });
 
 app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
+  console.log('Server started at http://localhost:' + port);
 });
 
