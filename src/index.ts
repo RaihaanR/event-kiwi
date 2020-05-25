65894082c2c54760561591ad5aaa79240b143c72
me@eugenel.in <me@eugenel.in>
2020-05-25 18:19:16 +0100

    [DE, EL] Added unit tests for database connection

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,16 @@
 import express from 'express';
-import pgPromise from 'pg-promise';
+
+import Database from './Database';
 
 const app = express();
 const port = process.env.PORT || 8080;
-
-const pgp = pgPromise();
-const dbOptions = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
-  database: process.env.DB_NAME,
-  ssl: true
-};
-const db = pgp(dbOptions);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/select1', (req, res) => {
-  db.any('SELECT * FROM test.table1')
+  Database.db().any('SELECT * FROM test.table1')
     .then((data: any) => {
       res.send(data);
     })
@@ -30,7 +20,7 @@ app.get('/select1', (req, res) => {
 });
 
 app.get('/select1/:col', (req, res) => {
-  db.any('SELECT $1:name FROM test.table1', [req.params['col']])
+  Database.db().any('SELECT $1:name FROM test.table1', [req.params['col']])
     .then((data: any) => {
       res.send(data);
     })
@@ -40,7 +30,7 @@ app.get('/select1/:col', (req, res) => {
 });
 
 app.get('/select2', (req, res) => {
-  db.any('SELECT * FROM test.table2')
+  Database.db().any('SELECT * FROM test.table2')
     .then((data: any) => {
       res.send(data);
     })
@@ -50,7 +40,7 @@ app.get('/select2', (req, res) => {
 });
 
 app.get('/select2/:col', (req, res) => {
-  db.any('SELECT $1:name FROM test.table2', [req.params['col']])
+  Database.db().any('SELECT $1:name FROM test.table2', [req.params['col']])
     .then((data: any) => {
       res.send(data);
     })
@@ -60,7 +50,7 @@ app.get('/select2/:col', (req, res) => {
 });
 
 app.get('/join', (req, res) => {
-  db.any('SELECT * FROM test.table1 NATURAL JOIN test.table2')
+  Database.db().any('SELECT * FROM test.table1 NATURAL JOIN test.table2')
     .then((data: any) => {
       res.send(data);
     })
