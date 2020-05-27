3eac1c040f178f31362a39871e8651ce6bb92886
Devesh Erda <devesh.erda18@imperial.ac.uk>
2020-05-27 20:17:51 +0100

    Added endpoint to get all event card details

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import express from 'express';
-
 import request from 'request-promise';
 
 import Bucket from './bucket';
 import Database from './database';
-import Sequences from './sequences';
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -13,91 +11,38 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/select/all/table1', async (req, res) => {
+app.get('/eventCardDetails', async (req, res) => {
   try {
-    res.send(await Database.allTable1());
-  } catch (error) {
+    res.send(await Database.getAllEvents());
+  } catch (err) {
     res.send('Error occurred');
-    console.log(error);
   }
 });
 
-app.get('/select/all/table2', async (req, res) => {
-  try {
-    res.send(await Database.allTable2());
-  } catch (error) {
-    res.send('Error occurred');
-    console.log(error);
-  }
-});
-
-app.get('/select/:col/table1', async (req, res) => {
-  try {
-    res.send(await Database.getColTable1(req.params.col));
-  } catch (error) {
-    res.send('Error occurred');
-    console.log(error);
-  }
-});
-
-app.get('/select/:col/table2', async (req, res) => {
-  try {
-    res.send(await Database.getColTable2(req.params.col));
-  } catch (error) {
-    res.send('Error occurred');
-    console.log(error);
-  }
-});
-
-app.get('/join', async (req, res) => {
-  try {
-    res.send(await Database.joinTable1Table2());
-  } catch (error) {
-    res.send('Error occurred');
-    console.log(error);
-  }
-});
-
-app.get("/factorial/:x", (req, res) => {
-  res.send(String(Sequences.factorial(+req.params.x)));
-});
-
-app.get("/fibonacci/:x", (req, res) => {
-  res.send(String(Sequences.fibonacci(+req.params.x)));
-});
-
-app.get("/mirror/:url", (req, res) => {
+app.get('/mirror/:url', (req, res) => {
   const options = {
     uri: req.params.url,
     encoding: null
   };
+
   request(options, (error, response, body) => {
     if (error || response.statusCode !== 200) {
-      res.send("Error (" + error + ") [" + response.statusCode + "]");
+      res.send('Error (' + error + ') [' + response.statusCode + ']');
     } else {
-      let params = {
+      const params = {
         Bucket: Bucket.bucketName(),
         Key: 'exampleFile',
         Body: body
       };
+
       Bucket.s3().putObject(params, (perr, pres) => {
         if (perr) {
-          res.send("Error! " + "--" + body + "--" + perr);
+          res.send('Error! ' + '--' + body + '--' + perr);
         } else {
-          res.send("Success!" + req.params.url);
+          res.send('Success!' + req.params.url);
         }
       });
     }
-  });
-});
-
-app.get("/btest", (req, res) => {
-  let params = {
-    Bucket: Bucket.bucketName(),
-    Key: '_test'
-  };
-  Bucket.s3().getObject(params, (err, data) => {
-    res.send(data.Body.toString('utf-8').trim() === "accessed!");
   });
 });
 
