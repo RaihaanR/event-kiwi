f87962f0b75b16ac65115fdd2112067360cb20d6
Devesh Erda <devesh.erda18@imperial.ac.uk>
2020-05-27 20:21:28 +0100

    Fixed test case to use new api

--- tests/database.test.ts
+++ tests/database.test.ts
@@ -2,9 +2,9 @@ import { expect } from 'chai';
 import Database from '../src/database';
 
 describe('table1', async function() {
-  it('first name should be \'Apple\'', async function() {
-    const data = await Database.getColTable1('name');
-    expect(data[0].name).equal("Apple");
+  it('event name should be \'DoCSoc - Introduction to Vim\'', async function() {
+    const data = await Database.getAllEvents();
+    expect(data[0].name).equal('DoCSoc - Introduction to Vim');
   });
 });
 
