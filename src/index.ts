91fc44d3d49244a3ec2e291f2cd9f1b0d299d2b5
Devesh Erda <devesh.erda18@imperial.ac.uk>
2020-05-28 18:21:06 +0100

    Using correct event resources column name

--- src/index.ts
+++ src/index.ts
@@ -74,7 +74,7 @@ app.get('/events/suggested/:id', async (req, res) => {
 app.get('/events/resources/:id', async (req, res) => {
   try {
     const event = await Database.db().oneOrNone('SELECT * FROM event WHERE event_id = $1', [+req.params.id]);
-    let resources = event.resources;
+    let resources = event['event_resources'];
     res.send(await Database.getFilesByIDs(resources));
   } catch (err) {
     res.send('Error occurred');
