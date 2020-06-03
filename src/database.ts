b17fca3ffc36b690ced1abb9400aa96dd433e3fc
Devesh Erda <devesh.erda18@imperial.ac.uk>
2020-06-03 21:02:36 +0100

    Formatted code to use consistent style

--- src/database.ts
+++ src/database.ts
@@ -82,8 +82,9 @@ export default class Database {
   static async searchEvents(query: any): Promise<any[]> {
     const values = {
       terms: query.split(' ').map(t => '%' + t + '%'),
-      search_term: query.replace(/\s/g, '|')
+      search_term: query.replace(/\s/g, '|'),
     };
+    values['length'] = values['terms'].length;
     const cards = await db.any(eventSQL.searchEvents, values);
 
     for (let i = 0; i < cards.length; i++) {
