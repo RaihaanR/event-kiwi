ce49395bc6b299a325f08e21a0812c9a2a721003
Devesh Erda <devesh.erda18@imperial.ac.uk>
2020-06-04 19:25:43 +0100

    Changed what interest search query returns

--- sql/profile/searchInterested.sql
+++ sql/profile/searchInterested.sql
@@ -1,14 +1,12 @@
 SELECT
-  (
-    SELECT
-      CASE WHEN ${search_term} ILIKE ANY("tags") THEN TRUE ELSE FALSE END
-    FROM
-      "interests"
-    WHERE
-      "user_id" = ${user_id}
-  ),
+  "tag",
   COUNT(*)
 FROM
   "interests"
+  CROSS JOIN UNNEST("tags") AS "tag"
 WHERE
-  to_tsvector(array_to_string("tags", ' ')) @@ to_tsquery(${pattern})
+  "tag" ILIKE ${pattern}
+GROUP BY
+  "tag"
+ORDER BY
+  "count" DESC
